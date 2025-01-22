ab83c5cf2227428f84bec02f9556eec2c2312906
Gabriel Paixão <78832303+paixaocode@users.noreply.github.com>
2025-01-22 11:26:19 -0300

    Atualizar o environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     baseUrlBackEnd: 'http://localhost:8080/',
-    geminiApiKey: 'AIzaSyDooIU-pA6zmRQOuJW5QstPryyttAogHH4',
+    geminiApiKey: '',
     urlApi: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='
 };
